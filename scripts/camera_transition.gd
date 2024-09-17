20fe60a4ec76591659195817452ec9e2bf7c99b7
evindor <cyanidesign@gmail.com>
2024-09-17 23:25:54 +0300

    tween the camera

--- scripts/camera_transition.gd
+++ scripts/camera_transition.gd
@@ -1,15 +1,14 @@
 extends Camera2D
 
 @export var room_size := Vector2 (160, 144)
-@export var transition_speed = 4.5
+@export var transition_speed = 0.7
 
 var target_position = Vector2 (0, 0)
 
 func _on_area_2d_camera_room_transition(movement_direction):
 	target_position += room_size * movement_direction
+	var tween = create_tween()
+	tween.tween_property(self, "position", target_position, transition_speed).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	force_move_player.emit(movement_direction)
-
-func _physics_process(delta):
-	position = position.lerp(target_position, delta * transition_speed) 
 
 signal force_move_player(Vector2)
