fb87c63d6d133c7925f20332fe8f57236f01e70f
Ewen-C <ewen.celibert@gmail.com>
2024-09-17 21:33:50 +0200

    Revert "Merge pull request #1 from Ewen-C/sfx"
    This reverts commit 4f1c4a051491dd29a6fd983f3b3b576e9acf7c1a, reversing
    changes made to 1511ee2357ad8283cc2a75b26562c4686806de73.

--- scripts/camera_transition.gd
+++ scripts/camera_transition.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
 @export var room_size := Vector2 (160, 144)
-@export var transition_speed = 6.5
+@export var transition_speed = 4.5
 
 var target_position = Vector2 (0, 0)
 
@@ -10,6 +10,6 @@ func _on_area_2d_camera_room_transition(movement_direction):
 	force_move_player.emit(movement_direction)
 
 func _physics_process(delta):
-	position = position.lerp(target_position, delta * transition_speed)
+	position = position.lerp(target_position, delta * transition_speed) 
 
 signal force_move_player(Vector2)
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @onready var timer = $TimerQuit
-@onready var music = $MusicPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
