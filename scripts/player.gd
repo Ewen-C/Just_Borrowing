fa19a211f1a79a2cf5bafedcd54a3f5bab92ac90
evindor <cyanidesign@gmail.com>
2024-09-18 13:43:24 +0300

    add steps sfx

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const TILE_SIZE = 8
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var raycast = $RayCast2D
+@onready var steps_sfx_player = $StepsSFX
 
 var is_moving = false
 var start_movement_position = Vector2(0, 0)
@@ -30,12 +31,16 @@ func process_new_input() :
 		
 		start_movement_position = position
 		is_moving = true
+		if !steps_sfx_player.playing:
+			steps_sfx_player.play()
 		
 		if movement_direction.x != 0 : animated_sprite.play("Walk_Right")
 		elif movement_direction.y == 1 : animated_sprite.play("Walk_Down")
 		else : animated_sprite.play("Walk_Up")
 		
 		animated_sprite.flip_h = (movement_direction.x == -1 && raycast_check_movement())
+	else:
+		steps_sfx_player.stop()
 	
 func move_player(delta) :
 	percent_to_next_tile += walk_speed * delta
