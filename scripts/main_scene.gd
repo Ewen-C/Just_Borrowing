1a2f56ac68a0929f7eeabd4189d7aae22026ab7e
Ewen-C <ewen.celibert@gmail.com>
2024-09-21 14:40:07 +0200

    Basic inventory with strikethough on collect

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -10,10 +10,12 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("action_start") : toogle_pause()
-	
+
 func toogle_pause() -> void: 
 	get_tree().paused = !get_tree().paused
+
 	if get_tree().paused: 
 		pause_menu.show();
 		pause_menu.current_items = game_manager.item_list
-	else: pause_menu.hide()
+	else: 
+		pause_menu.hide()
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,10 +5,18 @@ extends Control
 var current_items = {}
 
 
+# Reacreate labels with each _on_draw (to update strikethough)
 func _on_draw():
+	for item in current_items :
+		var my_label = RichTextLabel.new()
+		my_label.fit_content = true
+		if current_items[item] : my_label.push_strikethrough()
+		my_label.append_text(item)
+		my_label.add_theme_font_override("normal_font", load("res://assets/fonts/Quinquefive-ALoRM.ttf"))
+		my_label.add_theme_font_size_override("normal_font_size", 5)
+		my_label.add_theme_color_override("default_color", "#0f380f")
+		v_box_container.add_child(my_label)
+
+func _on_hidden():
 	for labels_items in v_box_container.get_children() :
 		labels_items.queue_free()
-	
-	for item in current_items :
-		print_debug(item)
-		print_debug(current_items[item])
