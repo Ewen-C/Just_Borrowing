a3aff8eabee1c481974ef7c2d752ab2fa6fda40f
Ewen-C <ewen.celibert@gmail.com>
2024-09-21 21:27:54 +0200

    Updated project to 4.3, added enemy & nav mesh

---
+++ scripts/enemy.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+const TILE_SIZE = 8
+
+@export var walk_speed = 5
+@export var chase_speed = 6.5
+@export var target: Vector2
+
+@onready var nav : NavigationAgent2D = $NavigationAgent2D
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var raycast : RayCast2D = $RayCast2D
+@onready var timer_zone_change : Timer = $TimerZoneChange
+@onready var timer_chase : Timer = $TimerChase
+
+var is_waiting = false
+var is_chasing_player = false
+var is_moving = false
+
+
+func _ready():
+	nav.target_position = Vector2.ZERO
+	timer_zone_change.start()
+	
+
+func _physics_process(delta):
+	if is_waiting :
+		if position.distance_to(target) > 0.5 :
+			var direction = (nav.get_next_path_position() - global_position).normalized()
+			if direction.x > direction.y :
+				print_debug("X") # Tile based movement
+			else :
+				print_debug("Y") # Tile based movement
+		else :
+			timer_zone_change.start()
+
+func _on_timer_zone_change_timeout():
+	# Get new target position
+	notify_position()
+
+func notify_position():
+	print_debug("New target : ")
+	# Print target position to UI
